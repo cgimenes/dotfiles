8773f20d524d1d4bfa424c8cfba403d119ea3ffa
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-02-01 17:44:08 -0300

    QoL

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -5,6 +5,9 @@ return {
       vim.keymap.set('n', '<leader>gs', '<cmd>Git<cr><C-w>o', { desc = '[G]it [S]tatus' })
       vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiff<cr>', { desc = '[G]it [D]iff' })
       vim.keymap.set('n', '<leader>gb', '<cmd>G blame<cr>', { desc = '[G]it [B]lame' })
-    end
+    end,
+    dependencies = {
+      "tpope/vim-rhubarb",
+    }
   }
 }
