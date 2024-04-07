db143676f060c153ba9186b3bba994bcce3b7f18
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-04-07 12:20:38 -0300

    Small linux updates

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,34 +1,11 @@
 return {
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
     dependencies = {
-      'nvim-tree/nvim-web-devicons'
+      'nvim-tree/nvim-web-devicons',
     },
     opts = function()
-      local function find_files()
-        require('telescope.builtin').find_files {
-          find_command = {
-            'rg',
-            '--files',
-            '--glob', '!**/pkg/*',
-            '--glob', '!**/.cache/*',
-            '--glob', '!**/.git/*',
-            '--glob', '!**/.npm/*',
-            '--glob', '!**/vendor/*',
-            '--glob', '!**/node_modules/*',
-            '--glob', '!**/.local/share/*',
-            '--hidden',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-          },
-        }
-      end
-
       vim.keymap.set('n', '<leader>;', '<cmd>Dashboard<cr>', { desc = 'Dashboard' }) -- I don't like this here
 
       local logo = [[
@@ -39,52 +16,47 @@ return {
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
       ]]
-      logo = string.rep("\n", 3) .. logo .. "\n\n"
+      logo = string.rep('\n', 3) .. logo .. '\n\n'
 
       local opts = {
-        theme = "doom",
+        theme = 'doom',
         hide = {
           -- this is taken care of by lualine
           -- enabling this messes up the actual laststatus setting after loading a file
           statusline = false,
         },
         config = {
-          header = vim.split(logo, "\n"),
+          header = vim.split(logo, '\n'),
           -- stylua: ignore
           center = {
-            { action = find_files,                                                 desc = " Find file",       icon = " ", key = "f" },
             { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-            { action = "Telescope live_grep",                                      desc = " Find text",       icon = "󰊄 ", key = "g" },
-            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
             { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
             { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
           },
           footer = function()
-            local stats = require("lazy").stats()
+            local stats = require('lazy').stats()
             local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
           end,
         },
       }
 
       for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 35 - #button.desc)
+        button.desc = button.desc .. string.rep(' ', 35 - #button.desc)
       end
 
       -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
+      if vim.o.filetype == 'lazy' then
         vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "DashboardLoaded",
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'DashboardLoaded',
           callback = function()
-            require("lazy").show()
+            require('lazy').show()
           end,
         })
       end
 
       return opts
     end,
-  }
+  },
 }
-
