b6eb7ae72f1ddf3eb00a6fe1a5ac8f343a7b8dfa
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-06-13 20:07:11 -0300

    Removing unused keymap

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -4,22 +4,14 @@ return {
     event = 'VeryLazy',
     opts = {},
     keys = {
-      { "gw", mode = { "n", "x", "o" }, function()
-        require("flash").jump({
-          pattern = ".", -- initialize pattern with any char
-          search = {
-            mode = function(pattern)
-              -- remove leading dot
-              if pattern:sub(1, 1) == "." then
-                pattern = pattern:sub(2)
-              end
-              -- return word pattern and proper skip pattern
-              return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-            end,
-          },
-        })
-      end, desc = "Flash Word" },
-      { "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      {
+        'gs',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
     },
   },
 }
