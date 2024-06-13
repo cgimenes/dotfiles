81cbc3555dd7f4dedad1cef87dc43a35510f00b3
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-06-13 20:07:16 -0300

    Better outline

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -203,6 +203,18 @@ return {
       modes = {
         symbols = {
           focus = true,
+          win = {
+            type = 'float',
+            relative = 'editor',
+            size = { width = 0.8, height = 0.8 },
+            position = { 0.5, 0.5 },
+            border = 'rounded',
+            title = 'Outline',
+            title_pos = 'center',
+          },
+          keys = {
+            ['<cr>'] = 'jump_close',
+          },
         },
       },
     },
