c9a070dfa6cddea60451b518d9f8c0bacb8ef91a
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-10-11 18:42:09 -0300

    Update 1-post-installation.sh

--- scripts/1-post-installation.sh
+++ scripts/1-post-installation.sh
@@ -10,7 +10,6 @@ rm -rf paru
 cd $HOME
 git clone https://github.com/cgimenes/dotfiles.git
 cd $HOME/dotfiles
-stow qtile
 stow nvim
 stow picom
 stow bspwm
@@ -22,12 +21,12 @@ stow zsh
 stow autorandr
 stow gsimplecal
 stow rofi
-stow lf
 stow dunst
 stow bin
 stow rg
 stow tmux
 stow gh-dash
+stow wezterm
 
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --import -
 paru -S --noconfirm spotify
@@ -40,8 +39,6 @@ paru -S --noconfirm slack-desktop
 paru -S --noconfirm insync
 
 rm -rf .config/autostart/**
-
-gh extension install dlvhdr/gh-dash
 
 gem install thor
 
