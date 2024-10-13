ff622342640d6cb9357fff8253645b76f98feb78
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-10-13 15:03:03 -0300

    Adding ChatGPT

--- nvim/.config/nvim/lua/plugins/abolish.lua
+++ nvim/.config/nvim/lua/plugins/abolish.lua
@@ -1,5 +1,7 @@
 return {
   {
+    -- :Subvert/child{,ren}/adult{,s}/g
+    -- snake_case (crs), MixedCase (crm), camelCase (crc), UPPER_CASE (cru), dash-case (cr-), and dot.case (cr.)
     'tpope/vim-abolish',
   },
 }
--- nvim/.config/nvim/lua/plugins/around-inside.lua
+++ nvim/.config/nvim/lua/plugins/around-inside.lua
@@ -8,6 +8,6 @@ return {
   {
     'echasnovski/mini.ai',
     event = 'VeryLazy',
-    config = {},
+    config = true,
   },
 }
