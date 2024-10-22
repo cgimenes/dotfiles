d7eb75c7fc27e459a42b64bf2834b80b2bba2867
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-10-22 12:26:19 -0300

    Better shell history

--- zsh/.zsh/0-env.zsh
+++ zsh/.zsh/0-env.zsh
@@ -11,6 +11,18 @@ export DISABLE_AUTO_UPDATE=true
 # Set Emacs mode
 bindkey -e
 
+# Better history
+setopt INC_APPEND_HISTORY   # History file is updated immediately after a command is entered
+setopt SHARE_HISTORY        # Allows multiple Zsh sessions to share the same command history 
+setopt APPENDHISTORY        # Ensures that each command entered in the current session is appended to the history file immediately after execution
+setopt HIST_IGNORE_DUPS     # Do not record an event that was just recorded again
+setopt HIST_FIND_NO_DUPS    # Do not display a previously found event
+setopt EXTENDED_HISTORY     # Write the history file in the ':start:elapsed;command' format
+HISTSIZE=5000000
+SAVEHIST=$HISTSIZE
+
+# Shell integrations
+
 export GOPATH="$HOME/dev"
 
 if [ -x "$(command -v /opt/homebrew/bin/brew)" ]; then
