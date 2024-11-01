56e8dc445f9a37c8d83879a3394aa018fc84ed5a
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-01 11:51:48 -0300

    Adding send_to_quickfix to oil.nvim

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -21,6 +21,7 @@ vim.api.nvim_create_autocmd('FileType', {
     'help',
     'fugitiveblame',
     'grug-far',
+    'qf',
   },
 })
 
@@ -35,4 +36,34 @@ vim.api.nvim_create_autocmd('FileType', {
       vim.opt.foldmethod = 'manual'
     end
   end,
+})
+
+-- Use ctrl-q to send oil selection to quickfix
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('oil-quickfix', { clear = true }),
+  callback = function(args)
+    vim.keymap.set({ 'n', 'v' }, '<C-q>', function()
+      local oil = require 'oil'
+
+      local line_number_start = vim.fn.line 'v'
+      local line_number_end = vim.fn.line '.'
+
+      local qf_list = {}
+
+      for i = line_number_start, line_number_end do
+        local filename = oil.get_current_dir(args.buf) .. oil.get_entry_on_line(args.buf, i).name
+        table.insert(qf_list, { filename = filename })
+      end
+
+      vim.fn.setqflist({}, ' ', {
+        nr = '$',
+        items = qf_list,
+        title = 'Oil',
+      })
+
+      oil.close()
+      vim.cmd 'botright copen'
+    end, { buffer = args.buf })
+  end,
+  pattern = { 'oil' },
 })
