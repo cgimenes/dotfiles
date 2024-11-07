b60373fe5a1e22b672f3208c34f8b4b545323c3e
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-07 16:06:00 -0300

    Adding oh-my-zsh settings and clipcopy/clippaste functions

--- zsh/.zsh/09-oh-my-zsh.zsh
+++ zsh/.zsh/09-oh-my-zsh.zsh
@@ -2,4 +2,19 @@
 source ~/.zsh/oh-my-zsh-plugins/sudo.plugin.zsh
 source ~/.zsh/oh-my-zsh-plugins/git.plugin.zsh
 
-alias ..='cd ..'
+# Changing/making/removing directory
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
