dd45b947dc720b9266321cd059b10e85bb7e2839
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-18 13:49:26 -0300

    Autostart Oil

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -68,3 +68,49 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
   pattern = { 'oil' },
 })
+
+-- Autostart oil
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = vim.api.nvim_create_augroup('oil-autostart', {}),
+  nested = true,
+  once = true,
+  callback = function()
+    -- Don't open if Neovim is opened to show something. That is
+    -- when at least one of the following is true:
+    -- - There are files in arguments (like `nvim foo.txt` with new file).
+    if vim.fn.argc() > 0 then
+      return
+    end
+
+    -- - Several buffers are listed (like session with placeholder buffers). That
+    --   means unlisted buffers (like from `nvim-tree`) don't affect decision.
+    local listed_buffers = vim.tbl_filter(function(buf_id)
+      return vim.fn.buflisted(buf_id) == 1
+    end, vim.api.nvim_list_bufs())
+    if #listed_buffers > 1 then
+      return
+    end
+
+    -- - Current buffer is meant to show something else
+    if vim.bo.filetype ~= '' then
+      return
+    end
+
+    -- - Current buffer has any lines (something opened explicitly).
+    -- NOTE: Usage of `line2byte(line('$') + 1) < 0` seemed to be fine, but it
+    -- doesn't work if some automated changed was made to buffer while leaving it
+    -- empty (returns 2 instead of -1). This was also the reason of not being
+    -- able to test with child Neovim process from 'tests/helpers'.
+    local n_lines = vim.api.nvim_buf_line_count(0)
+    if n_lines > 1 then
+      return
+    end
+    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
+    if string.len(first_line) > 0 then
+      return
+    end
+
+    vim.cmd 'Oil'
+  end,
+  desc = 'Open on VimEnter',
+})
