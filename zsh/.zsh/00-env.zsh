9040e9f4e78a6d4fa20a1d4dd6ad1e4df761c1d7
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-20 19:56:24 -0300

    Adding delta

--- zsh/.zsh/00-env.zsh
+++ zsh/.zsh/00-env.zsh
@@ -2,6 +2,7 @@ export TERM="xterm-256color"
 export HISTORY_IGNORE="(ls|cd|pwd|exit|sudo reboot|history|cd -|cd ..)"
 export EDITOR="nvim"
 export VISUAL="nvim"
+export PAGER="less -R"
 export MANPAGER="nvim +Man!"
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/rg/config"
 export DISABLE_AUTO_UPDATE=true # Disable oh-my-zsh auto-update
