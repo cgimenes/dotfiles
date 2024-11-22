0a2129dd0966d49bc5ceef227fc26f190f3f46a7
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-22 03:11:32 -0300

    Better copy-mode-vi in tmux

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -21,6 +21,7 @@ return {
       local opts = {
         theme = 'doom',
         hide = {
+          tabline = false,
           -- this is taken care of by lualine
           -- enabling this messes up the actual laststatus setting after loading a file
           statusline = false,
