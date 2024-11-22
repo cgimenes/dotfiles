f2b2ecd2bc1f49545f08dc927dc597ee2b84b68d
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-22 17:41:07 -0300

    Switching from oil.nvim to yazi.nvim

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -7,8 +7,10 @@ return {
         ['<C-t>'] = false,
         ['<C-h>'] = false,
         ['<C-l>'] = false,
+        ['<C-/>'] = 'actions.cd',
         ['<M-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-        ['<M-s>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<M-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<M-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in a new tab' },
       },
       skip_confirm_for_simple_edits = true,
       view_options = {
@@ -23,8 +25,31 @@ return {
     },
     cmd = 'Oil',
     keys = {
-      { '-', '<cmd>Oil<cr>', desc = 'Oil [E]xplorer' },
-      { '<leader>-', '<cmd>Oil --float<cr>', desc = 'Floating Oil [E]xplorer' },
+      { '<leader>-', '<cmd>Oil --float<cr>', desc = 'Oil Explorer' },
+    },
+  },
+  {
+    'mikavilpas/yazi.nvim',
+    keys = {
+      { '-', '<cmd>Yazi<cr>', desc = 'Yazi Explorer' },
+    },
+    opts = {
+      open_for_directories = true,
+      keymaps = {
+        open_file_in_vertical_split = '<m-v>',
+        open_file_in_horizontal_split = '<m-h>',
+        open_file_in_tab = '<m-t>',
+        grep_in_directory = '<c-g>',
+        replace_in_directory = '<c-r>',
+        copy_relative_path_to_selected_files = '<c-y>',
+        send_to_quickfix_list = '<c-q>',
+        change_working_directory = '<c-/>',
+      },
+      integrations = {
+        grep_in_directory = function(directory)
+          require('fzf-lua').live_grep { cwd = directory }
+        end,
+      },
     },
   },
 }
