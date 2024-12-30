58e4f4d120d7a3a0d1b097b6a13757494f4cad2c
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-12-30 09:59:56 -0300

    Removing noice

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -23,46 +23,6 @@ return {
     event = { 'WinLeave' },
   },
   {
-    -- Better cmdline, popupmenu and messages
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-      messages = {
-        view_search = false,
-      },
-    },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      {
-        'rcarriga/nvim-notify',
-        opts = {
-          timeout = 2000,
-          stages = 'static',
-          render = 'wrapped-compact',
-          max_width = 50,
-        },
-      },
-    },
-    keys = {
-      { '<leader>N', '<cmd>NoiceHistory<cr>', desc = '[N]otification history' },
-    },
-  },
-  {
     -- Better vim.select
     'stevearc/dressing.nvim',
     opts = {
