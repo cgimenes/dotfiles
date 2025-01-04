007933be5f858fee4edfc45ed23bcfac04a29b3b
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-01-04 20:31:19 -0300

    Overseer watch

--- nvim/.config/nvim/lua/plugins/runner.lua
+++ nvim/.config/nvim/lua/plugins/runner.lua
@@ -3,6 +3,7 @@ return {
     'stevearc/overseer.nvim',
     opts = {
       dap = false,
+      templates = { 'builtin', 'user.run_script' },
       task_list = {
         bindings = {
           ['K'] = 'ScrollOutputUp',
@@ -15,6 +16,23 @@ return {
     keys = {
       { '<leader>rr', '<cmd>OverseerRun<cr>', desc = 'Overseer [R]un' },
       { '<leader>rt', '<cmd>OverseerToggle<cr>', desc = 'Overseer [T]oggle' },
+      {
+        '<leader>rw',
+        function()
+          local overseer = require 'overseer'
+          overseer.run_template({ name = 'run script' }, function(task)
+            if task then
+              task:add_component { 'restart_on_save', paths = { vim.fn.expand '%:p' } }
+              local main_win = vim.api.nvim_get_current_win()
+              overseer.run_action(task, 'open vsplit')
+              vim.api.nvim_set_current_win(main_win)
+            else
+              vim.notify('WatchRun not supported for filetype ' .. vim.bo.filetype, vim.log.levels.ERROR)
+            end
+          end)
+        end,
+        desc = 'Overseer [W]atch',
+      },
     },
   },
 }
