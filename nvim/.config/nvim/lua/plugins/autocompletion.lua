382da286f4e5f96a03aed1e89d95366f8118f414
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-02-15 20:05:37 -0300

    Updating blink.cmp

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -4,23 +4,13 @@ return {
     -- !Important! Make sure you're using the latest release of LuaSnip
     -- `main` does not work at the moment
     dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+    version = '*',
     opts = {
       snippets = {
-        expand = function(snippet)
-          require('luasnip').lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction)
-          require('luasnip').jump(direction)
-        end,
+        preset = 'luasnip',
       },
       sources = {
-        default = { 'lsp', 'path', 'luasnip', 'buffer', 'dadbod', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'dadbod', 'lazydev' },
         providers = {
           -- dont show LuaLS require statements when lazydev has items
           lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', fallbacks = { 'lsp' } },
