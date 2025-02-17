fed865135b1112c4e368384c4ebc26a1d416bc3d
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-02-17 17:52:48 -0300

    Moving indent-blankline to appearance file

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -35,4 +35,19 @@ return {
       },
     },
   },
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = "▏",
+      },
+      scope = {
+        enabled = false,
+      }
+    },
+  },
 }
