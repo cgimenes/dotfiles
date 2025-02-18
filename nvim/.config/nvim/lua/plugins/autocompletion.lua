7380f619f1f0abd5fee3a3104673b3318d73e734
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-02-18 17:42:45 -0300

    Decreasing documentation delay

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -27,6 +27,7 @@ return {
       completion = {
         documentation = {
           auto_show = true,
+          auto_show_delay_ms = 0,
           window = { border = 'single' },
         },
         menu = {
