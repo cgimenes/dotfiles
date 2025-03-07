d1f774be13510313fdd3224611c45cceba254f25
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-03-07 11:16:22 -0300

    Adding oil back

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -1,5 +1,34 @@
 return {
   {
+    'stevearc/oil.nvim',
+    opts = {
+      keymaps = {
+        ['q'] = 'actions.close',
+        ['<C-t>'] = false,
+        ['<C-h>'] = false,
+        ['<C-l>'] = false,
+        ['<C-/>'] = 'actions.cd',
+        ['<M-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+        ['<M-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<M-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in a new tab' },
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        is_hidden_file = function(name)
+          return vim.startswith(name, '.') or name == '__pycache__'
+        end,
+      },
+      float = {
+        padding = 5,
+        max_width = 100,
+      },
+    },
+    cmd = 'Oil',
+    keys = {
+      { '<leader>-', '<cmd>Oil --float<cr>', desc = 'Oil Explorer' },
+    },
+  },
+  {
     'mikavilpas/yazi.nvim',
     keys = {
       { '-', '<cmd>Yazi<cr>', desc = 'Yazi Explorer' },
