a1dd8b5d31e493fece688dc922be7e60f086e721
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-03-12 10:59:34 -0300

    Improving wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,20 +1,22 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
+
 config.enable_wayland = false
 config.front_end = "OpenGL"
 config.hide_tab_bar_if_only_one_tab = true
 config.force_reverse_video_cursor = true
 config.colors = require("kanagawa")
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 2,
 }
 config.initial_rows = 39
 config.initial_cols = 126
-config.window_decorations = "TITLE|RESIZE|MACOS_FORCE_DISABLE_SHADOW"
 config.font = wezterm.font_with_fallback({
 	{ family = "Iosevka Term", weight = "Regular" },
 	{ family = "JetBrains Mono", weight = "Medium" },
@@ -22,7 +24,6 @@ config.font = wezterm.font_with_fallback({
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 1
-config.macos_window_background_blur = 15
 
 config.keys = {
 	{
@@ -32,11 +33,15 @@ config.keys = {
 	},
 }
 
-local hostname = wezterm.hostname()
-if hostname == 'archlinux' then
+if is_linux then
 	config.font_size = 14.0
-else
+	config.window_decorations = "NONE"
+end
+
+if is_darwin then
 	config.font_size = 18.0
+	config.window_decorations = "TITLE|RESIZE|MACOS_FORCE_DISABLE_SHADOW"
+	config.macos_window_background_blur = 15
 end
 
 return config
