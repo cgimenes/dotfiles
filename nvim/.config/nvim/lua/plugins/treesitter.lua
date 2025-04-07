45147645a7467e8daf226155a52806005cbaef8b
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-04-07 16:13:30 -0300

    Small improvements

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -45,13 +45,21 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+      },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    -- Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    -- keymaps:
+    --   - init_selection: in normal mode, start incremental selection.
+    --     Defaults to `gnn`.
+    --   - node_incremental: in visual mode, increment to the upper named parent.
+    --     Defaults to `grn`.
+    --   - scope_incremental: in visual mode, increment to the upper scope
+    --     (as defined in `locals.scm`). Defaults to `grc`.
+    --   - node_decremental: in visual mode, decrement to the previous named node.
+    --     Defaults to `grm`.
+    -- Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
