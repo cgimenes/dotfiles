7c90c850a8d51bc3d3e4ee18ccf3640a21bb238e
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-04-08 14:17:35 -0300

    Trying compiler.nvim

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -1,2 +1,8 @@
 return {
+  {
+    'Zeioth/compiler.nvim',
+    cmd = { 'CompilerOpen', 'CompilerToggleResults', 'CompilerRedo' },
+    dependencies = { 'stevearc/overseer.nvim' },
+    opts = {},
+  },
 }
