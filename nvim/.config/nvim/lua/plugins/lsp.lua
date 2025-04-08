45623cab5a5ed3f722bb553a8afe8908faf5def9
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-04-08 10:24:31 -0300

    Back to blink.cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -164,7 +164,9 @@ return {
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      if package.loaded['blink.cmp'] then
+        capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
+      end
       require('mason-lspconfig').setup {
         ensure_installed = {},
         automatic_installation = false,
