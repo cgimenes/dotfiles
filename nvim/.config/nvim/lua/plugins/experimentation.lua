a2e89d70a18db4c7b8b3241de2f8349f9b7228bc
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-04-08 17:01:13 -0300

    Trying nvim-scissors

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -10,4 +10,28 @@ return {
     -- start insert mode with i,a,I,A
     'mg979/vim-visual-multi',
   },
+  {
+    'chrisgrieser/nvim-scissors',
+    lazy = false,
+    opts = {
+      snippetDir = vim.fn.stdpath 'config' .. '/snippets',
+    },
+    keys = {
+      {
+        '<leader>cse',
+        function()
+          require('scissors').editSnippet()
+        end,
+        desc = 'Edit Snippet',
+      },
+      {
+        '<leader>csa',
+        function()
+          require('scissors').addNewSnippet()
+        end,
+        desc = 'Add Snippet',
+        mode = { 'n', 'x' },
+      },
+    },
+  },
 }
