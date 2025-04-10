0d97384fb40c33006ed7215c61066eafde8d595a
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-04-09 23:39:16 -0300

    Adding trigger text for snippets

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -1,3 +1,5 @@
+local trigger_text = ';'
+
 return {
   {
     'saghen/blink.cmp',
@@ -24,8 +26,41 @@ return {
           copilot = {
             name = 'Copilot',
             module = 'blink-copilot',
-            score_offset = 100,
+            score_offset = 90,
             async = true,
+          },
+          snippets = {
+            score_offset = 100,
+            -- Only show snippets if I type the trigger_text characters
+            should_show_items = function()
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+              return before_cursor:match(trigger_text .. '%w*$') ~= nil
+            end,
+            -- After accepting the completion, delete the trigger_text characters
+            transform_items = function(_, items)
+              local line = vim.api.nvim_get_current_line()
+              local col = vim.api.nvim_win_get_cursor(0)[2]
+              local before_cursor = line:sub(1, col)
+              local start_pos, end_pos = before_cursor:find(trigger_text .. '[^' .. trigger_text .. ']*$')
+              if start_pos then
+                for _, item in ipairs(items) do
+                  if not item.trigger_text_modified then
+                    ---@diagnostic disable-next-line: inject-field
+                    item.trigger_text_modified = true
+                    item.textEdit = {
+                      newText = item.insertText or item.label,
+                      range = {
+                        start = { line = vim.fn.line '.' - 1, character = start_pos - 1 },
+                        ---@diagnostic disable-next-line: assign-type-mismatch
+                        ['end'] = { line = vim.fn.line '.' - 1, character = end_pos },
+                      },
+                    }
+                  end
+                end
+              end
+              return items
+            end,
           },
         },
       },
