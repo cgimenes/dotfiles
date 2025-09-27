a2dc885adac308f20b03d5435f99f2a3a039673c
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-09-27 18:03:17 -0300

    Removing unused snacks layout

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -59,23 +59,6 @@ return {
           cycle = true,
         },
         layouts = {
-          default = {
-            hidden = { 'preview' },
-            layout = {
-              box = 'horizontal',
-              width = 0.8,
-              min_width = 120,
-              height = 0.8,
-              {
-                box = 'vertical',
-                border = 'rounded',
-                title = '{title} {live} {flags}',
-                { win = 'input', height = 1, border = 'bottom' },
-                { win = 'list', border = 'none' },
-              },
-              { win = 'preview', title = '{preview}', border = 'rounded', width = 0.5 },
-            },
-          },
           ivy = {
             hidden = { 'preview' },
             layout = {
