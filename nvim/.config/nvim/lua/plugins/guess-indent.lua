4e24f37df2c5031d64ad1a762d8ef1c4edc8e6ec
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-10-14 21:12:40 -0300

    Back to vim-sleuth

--- nvim/.config/nvim/lua/plugins/guess-indent.lua
+++ nvim/.config/nvim/lua/plugins/guess-indent.lua
@@ -2,9 +2,8 @@ return {
   {
     -- Detect tabstop and shiftwidth automatically
     {
-      'NMAC427/guess-indent.nvim',
+      'tpope/vim-sleuth',
       event = 'VeryLazy',
-      config = true,
     },
   },
 }
