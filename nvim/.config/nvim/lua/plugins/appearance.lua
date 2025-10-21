d0bf91ec40d1247d55c621a5b7b8a6f88eccc9e4
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-10-21 11:03:45 -0300

    Promoting tiny-inline-diagnostic.nvim

--- nvim/.config/nvim/lua/plugins/appearance.lua
+++ nvim/.config/nvim/lua/plugins/appearance.lua
@@ -18,6 +18,17 @@ return {
     },
   },
   {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    opts = {
+      preset = 'classic',
+      options = {
+        show_all_diags_on_cursorline = true,
+      },
+    },
+  },
+  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -26,15 +26,4 @@ return {
     },
     cmd = 'Store',
   },
-  {
-    'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'VeryLazy',
-    priority = 1000,
-    opts = {
-      preset = 'classic',
-      options = {
-        show_all_diags_on_cursorline = true,
-      },
-    },
-  },
 }
