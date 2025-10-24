f9f8f81ad6376fd2305e1a5dab6be9c4739dd5ad
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-10-24 19:52:52 -0300

    Adding another overseer keymap

--- nvim/.config/nvim/lua/overseer/component/options.lua
+++ nvim/.config/nvim/lua/overseer/component/options.lua
@@ -5,6 +5,7 @@ return {
     return {
       on_start = function(_, task)
         vim.keymap.set('n', 'gf', '<c-w>sgF<c-w>H<c-w>l<c-w>q', { buffer = task:get_bufnr() })
+        vim.keymap.set('n', 'q', '<c-w>q', { buffer = task:get_bufnr() })
       end,
     }
   end,
