9a11f88edd0653065dace8c9e74f0d685b11678f
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-10-30 22:22:45 -0300

    Promoting toggleterm.nvim

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -1,22 +1,3 @@
--- toggleterm
-vim.pack.add {
-  { src = 'https://github.com/akinsho/toggleterm.nvim', version = vim.version.range '*' },
-}
-require('toggleterm').setup {
-  size = function(term)
-    if term.direction == 'horizontal' then
-      return 10
-    elseif term.direction == 'vertical' then
-      return vim.o.columns * 0.4
-    else
-      return 20
-    end
-  end,
-  auto_scroll = false,
-}
-Map { '<leader>.', '<cmd>ToggleTerm<cr>', desc = 'Toggle Terminal' }
-Map { '<leader>>', '<cmd>ToggleTerm direction=vertical<cr>', desc = 'Toggle Vertical Terminal' }
-
 -- <M-h> <M-j> <M-k> <M-l> to move lines and blocks
 vim.pack.add { 'https://github.com/nvim-mini/mini.move' }
 require('mini.move').setup {}
