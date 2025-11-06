5949a7d816d43fc121bb06e589b50de4c9400e31
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-11-06 09:04:07 -0300

    Removing most of mini.bracketed keymaps

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -4,11 +4,22 @@ require('mini.move').setup {}
 
 -- Yank selection replacing latest put region [Y [y ]y ]Y
 -- File on disk [F [f ]f ]F
--- Comment block [C [c ]c ]C
--- Conflict marker [X [x ]x ]X
 vim.pack.add { 'https://github.com/nvim-mini/mini.bracketed' }
 require('mini.bracketed').setup {
+  file = { suffix = 'f' },
+  yank = { suffix = 'y' },
+  buffer = { suffix = '' },
+  comment = { suffix = '' },
+  conflict = { suffix = '' },
+  diagnostic = { suffix = '' },
+  indent = { suffix = '' },
+  jump = { suffix = '' },
+  location = { suffix = '' },
+  oldfile = { suffix = '' },
   quickfix = { suffix = '' },
+  treesitter = { suffix = '' },
+  undo = { suffix = '' },
+  window = { suffix = '' },
 }
 
 -- Set a different colorscheme per filetype
