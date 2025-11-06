9060da4641ffdc4f93593405a5cfe64d6bd8ce7f
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-11-06 17:47:39 -0300

    Removing duplicated mouse menu item

--- nvim/.config/nvim/lua/mouse.lua
+++ nvim/.config/nvim/lua/mouse.lua
@@ -1,7 +1,6 @@
 vim.cmd [[
   aunmenu PopUp.How-to\ disable\ mouse
 
-  anoremenu PopUp.Definition  <cmd>lua vim.lsp.buf.definition()<cr>
   anoremenu PopUp.Back        <C-o>
   anoremenu PopUp.Forward     <C-i>
 ]]
