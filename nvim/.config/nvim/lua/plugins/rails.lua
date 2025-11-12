dd056a87c698e0789c6cda676eaee9a3a2137356
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-11-12 00:29:33 -0300

    Fix eruby.yaml filetype

--- nvim/.config/nvim/lua/plugins/rails.lua
+++ nvim/.config/nvim/lua/plugins/rails.lua
@@ -2,3 +2,8 @@
 -- Go to file -> gf
 vim.pack.add { 'https://github.com/tpope/vim-rails' }
 Map { '<leader>crr', '<cmd>R<cr>', desc = 'Rails Related File' }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'eruby.yaml',
+  command = 'set filetype=yaml',
+})
