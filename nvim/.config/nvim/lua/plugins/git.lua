17212df42c4d8f23badfaeb13734d847fa320ae9
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-12-09 12:42:09 -0300

    Replacing gitsigns.nvim with mini.diff

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -2,7 +2,7 @@
 vim.pack.add { 'https://github.com/tpope/vim-fugitive' }
 Map { '<leader>gb', '<cmd>G blame<cr>', desc = 'Git Blame' }
 Map { '<leader>gc', '<cmd>G mergetool<cr>', desc = 'List Git Conflicts' }
-Map { '<leader>gd', '<cmd>vertical G diff<cr>', desc = 'Git Diff' }
+Map { '<leader>gd', '<cmd>vertical G diff %<cr>', desc = 'Git Diff Current File' }
 
 -- Diffview
 vim.pack.add { 'https://github.com/sindrets/diffview.nvim' }
@@ -10,75 +10,11 @@ require('diffview').setup {}
 Map { '<leader>gh', ':DiffviewFileHistory %<cr>', desc = 'File History' }
 Map { '<leader>gD', '<cmd>DiffviewOpen<cr>', desc = 'Git Diffview' }
 
--- Gitsigns
-vim.pack.add { 'https://github.com/lewis6991/gitsigns.nvim' }
-require('gitsigns').setup {
-  update_debounce = 500,
-  on_attach = function(bufnr)
-    local gitsigns = require 'gitsigns'
-
-    vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk_inline, { buffer = bufnr, desc = 'Preview hunk' })
-    vim.keymap.set('n', '<leader>hy', function()
-      local cache = require('gitsigns.cache').cache
-      local Hunks = require 'gitsigns.hunks'
-
-      local bcache = cache[bufnr]
-      if not bcache then
-        return
-      end
-
-      local hunk, _ = bcache:get_cursor_hunk()
-      if not hunk then
-        return
-      end
-
-      local patch = Hunks.patch_lines(hunk, vim.bo[bufnr].fileformat)
-      vim.fn.setreg('+', patch)
-    end, { buffer = bufnr, desc = 'Yank hunk' })
-    vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, { buffer = bufnr, desc = 'Stage buffer' })
-    vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { buffer = bufnr, desc = 'Stage hunk' })
-    vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { buffer = bufnr, desc = 'Reset hunk' })
-    vim.keymap.set('x', '<leader>hs', function()
-      gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-    end, { buffer = bufnr, desc = 'Stage hunk' })
-    vim.keymap.set('x', '<leader>hr', function()
-      gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-    end, { buffer = bufnr, desc = 'Reset hunk' })
-
-    vim.keymap.set({ 'o', 'x' }, 'ih', '<Cmd>Gitsigns select_hunk<CR>')
-
-    vim.keymap.set('n', ']c', function()
-      if vim.wo.diff then
-        return ']c'
-      end
-      vim.schedule(function()
-        gitsigns.nav_hunk 'next'
-      end)
-      return '<Ignore>'
-    end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-    vim.keymap.set('n', '[c', function()
-      if vim.wo.diff then
-        return '[c'
-      end
-      vim.schedule(function()
-        gitsigns.nav_hunk 'prev'
-      end)
-      return '<Ignore>'
-    end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-  end,
+-- Git signs
+vim.pack.add { 'https://github.com/nvim-mini/mini.diff' }
+require('mini.diff').setup {
+  view = { style = 'sign', signs = { add = '┃', change = '┃', delete = '▁' } },
 }
-
--- gp - open the workflow file below the cursor on GitHub
--- gr - open the workflow run below the cursor on GitHub
--- gj - open the job of the workflow run below the cursor on GitHub
--- d - dispatch a new run for the workflow below the cursor on GitHub
-vim.pack.add {
-  'https://github.com/MunifTanjim/nui.nvim',
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/topaxi/pipeline.nvim',
-}
-require('pipeline').setup {}
-Map { '<leader>ga', '<cmd>Pipeline<cr>', desc = 'Open Github Actions' }
 
 -- Octo.nvim (GitHub PRs and Issues)
 vim.pack.add {
