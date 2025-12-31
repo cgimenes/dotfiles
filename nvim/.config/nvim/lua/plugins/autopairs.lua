14961a0a6555330021cdcd5dedc1b7571f79ec03
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2025-12-31 15:58:33 -0300

    Improving autopairs

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -3,19 +3,10 @@
 --   bs = { delete_from_end = false },
 -- }
 
--- vim.pack.add { 'https://github.com/m4xshen/autoclose.nvim' }
--- require('autoclose').setup {}
---
--- vim.pack.add { 'https://github.com/nvim-mini/mini.pairs' }
--- require('mini.pairs').setup {}
---
--- vim.pack.add { 'https://github.com/windwp/nvim-autopairs' }
--- require('nvim-autopairs').setup {}
-
 -- Adds "end" in Ruby, etc.
 vim.pack.add { 'https://github.com/tpope/vim-endwise' }
 
--- Closes brackets, parentheses, quotes, etc. On <CR> and on <C-P>
+-- Closes brackets, parentheses, quotes, etc. On <CR> and on <C-O>
 vim.pack.add { 'https://github.com/rstacruz/vim-closer' }
-vim.g.pairify_map = '<C-P>'
+vim.g.pairify_map = '<C-O>'
 vim.pack.add { 'https://github.com/dhruvasagar/vim-pairify' }
