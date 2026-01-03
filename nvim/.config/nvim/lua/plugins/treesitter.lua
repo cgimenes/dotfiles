4b6d490ddda8031fa4af15d8ae55414c0dcca05a
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-01-03 14:18:38 -0300

    Adding treesitter back

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,24 @@
 -- Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-local languages = {}
 vim.pack.add { { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'master' } }
 require('nvim-treesitter.configs').setup {
-  ensure_installed = languages,
   auto_install = false,
-  highlight = { enable = languages },
-  indent = { enable = languages },
+  ensure_installed = {
+    'tsx',
+    'typescript',
+  },
+  highlight = { enable = false },
+  indent = { enable = false },
 }
 BuildAfterUpdate('nvim-treesitter', ':TSUpdate')
 
 -- setting the commentstring option based on the cursor location in the file.
--- vim.pack.add { 'https://github.com/JoosepAlviste/nvim-ts-context-commentstring' }
--- require('ts_context_commentstring').setup { enable_autocmd = false }
--- local get_option = vim.filetype.get_option
--- vim.filetype.get_option = function(filetype, option)
---   return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
--- end
+vim.pack.add { 'https://github.com/JoosepAlviste/nvim-ts-context-commentstring' }
+require('ts_context_commentstring').setup { enable_autocmd = false }
+local get_option = vim.filetype.get_option
+vim.filetype.get_option = function(filetype, option)
+  return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+end
 
 -- Use treesitter to autoclose and autorename html tag
--- vim.pack.add { 'https://github.com/windwp/nvim-ts-autotag' }
--- require('nvim-ts-autotag').setup {}
+vim.pack.add { 'https://github.com/windwp/nvim-ts-autotag' }
+require('nvim-ts-autotag').setup {}
