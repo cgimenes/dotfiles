1a98c49b6553864d7216f0a340876e76b8ac31e1
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-01-04 15:17:50 -0300

    Better FixJson user command

--- nvim/.config/nvim/lua/usercmd.lua
+++ nvim/.config/nvim/lua/usercmd.lua
@@ -1,11 +1,23 @@
 vim.api.nvim_create_user_command('FixJson', function()
-  vim.opt_local.filetype = 'json'
+  vim.cmd 'silent! %s/^"/'
+  vim.cmd 'silent! %s/"$/'
+  local buf = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  local input = table.concat(lines, '\n')
 
-  vim.cmd 'silent! %s/\\\\"/"/g<cr>'
-  vim.cmd 'silent! %s/^"//<cr>'
-  vim.cmd 'silent! %s/"$//<cr>'
-  vim.cmd 'silent! %s/"$//<cr>'
-  require('conform').format {}
+  -- Wrap input in quotes so jq can parse it as a JSON string
+  local wrapped = '"' .. input .. '"'
+
+  local result = vim.system({ 'jq', '-r', 'fromjson' }, { stdin = wrapped, text = true }):wait()
+
+  if result.code ~= 0 then
+    vim.notify('jq error:\n' .. (result.stderr or ''), vim.log.levels.ERROR)
+    return
+  end
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result.stdout, '\n', { trimempty = true }))
+
+  vim.bo.filetype = 'json'
 end, {})
 
 vim.api.nvim_create_user_command('OpenPathOnWindow', function()
