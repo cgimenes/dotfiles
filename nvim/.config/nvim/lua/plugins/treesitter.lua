4e626d07e96dbfb2abf7eaf7fc2ef2baed49f2d6
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-01-11 14:59:27 -0300

    Many small changes

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ vim.pack.add { { src = 'https://github.com/nvim-treesitter/nvim-treesitter', ver
 require('nvim-treesitter.configs').setup {
   auto_install = false,
   ensure_installed = {
+    'javascript',
     'tsx',
     'typescript',
   },
