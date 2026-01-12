4e4f8d525b68d28e2a66528fc9d801a98218aa0b
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-01-12 13:02:15 -0300

    Removing lazy load for nvim-navbuddy

--- nvim/.config/nvim/lua/plugins/outline.lua
+++ nvim/.config/nvim/lua/plugins/outline.lua
@@ -1,17 +1,10 @@
-LazyLoad {
-  'nvim-navbuddy',
-  after = function()
-    vim.pack.add {
-      'https://github.com/SmiteshP/nvim-navic',
-      'https://github.com/MunifTanjim/nui.nvim',
-      'https://github.com/hasansujon786/nvim-navbuddy',
-    }
-    require('nvim-navbuddy').setup {
-      lsp = { auto_attach = true },
-      window = { size = '90%' },
-    }
-  end,
-  keys = {
-    { '<leader>lo', '<cmd>Navbuddy root<cr>', desc = 'Outline' },
-  },
+vim.pack.add {
+  'https://github.com/SmiteshP/nvim-navic',
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/hasansujon786/nvim-navbuddy',
 }
+require('nvim-navbuddy').setup {
+  lsp = { auto_attach = true },
+  window = { size = '90%' },
+}
+Map { '<leader>lo', '<cmd>Navbuddy root<cr>', desc = 'Outline' }
