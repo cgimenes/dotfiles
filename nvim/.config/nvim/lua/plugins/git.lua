6b66269f9dd09432b0dff330dfb8484aed7a6de9
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-01-19 16:29:13 -0300

    unclash.nvim traverses the entire cwd even if not in a git repo

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 -- Fugitive
 vim.pack.add { 'https://github.com/tpope/vim-fugitive' }
 Map { '<leader>gb', '<cmd>G blame<cr>', desc = 'Git Blame' }
--- Map { '<leader>gc', '<cmd>G mergetool<cr>', desc = 'List Git Conflicts' }
+Map { '<leader>gc', '<cmd>G mergetool<cr>', desc = 'List Git Conflicts' }
 Map { '<leader>gd', '<cmd>vertical G diff HEAD %<cr>', desc = 'Git Diff Current File' }
 
 -- Diffview
@@ -38,59 +38,42 @@ Map { '<leader>gpr', '<cmd>Octo review<cr>', desc = 'Review' }
 Map { '<leader>gpt', '<cmd>Octo review thread<cr>', desc = 'Show Thread' }
 
 -- Conflicts
-vim.pack.add { 'https://github.com/madmaxieee/unclash.nvim' }
-local unclash = require 'unclash'
-Map { ']x', unclash.next_conflict, desc = 'Next Conflict' }
-Map { '[x', unclash.prev_conflict, desc = 'Prev Conflict' }
-Map { '<leader>gco', unclash.open_merge_editor, desc = 'Open Merge Editor' }
-Map { '<leader>gcc', unclash.accept_current, desc = 'Accept Current' }
-Map { '<leader>gci', unclash.accept_incoming, desc = 'Accept Incoming' }
-Map { '<leader>gcb', unclash.accept_both, desc = 'Accept Both' }
-Map {
-  '<leader>gcq',
-  function()
-    unclash.set_qflist()
-    vim.cmd 'copen'
-  end,
-  desc = 'List Git Conflicts',
-}
+vim.pack.add { {
+  src = 'https://github.com/akinsho/git-conflict.nvim',
+  version = vim.version.range '*',
+} }
+require('git-conflict').setup { default_mappings = false, default_commands = false }
+local group = vim.api.nvim_create_augroup('GitConflictKeymap', { clear = true })
+vim.api.nvim_create_autocmd('User', {
+  group = group,
+  pattern = 'GitConflictDetected',
+  callback = function(event)
+    vim.diagnostic.enable(false, { bufnr = event.buf })
 
--- vim.pack.add { {
---   src = 'https://github.com/akinsho/git-conflict.nvim',
---   version = vim.version.range '*',
--- } }
--- require('git-conflict').setup { default_mappings = false, default_commands = false }
--- local group = vim.api.nvim_create_augroup('GitConflictKeymap', { clear = true })
--- vim.api.nvim_create_autocmd('User', {
---   group = group,
---   pattern = 'GitConflictDetected',
---   callback = function(event)
---     vim.diagnostic.enable(false, { bufnr = event.buf })
---
---     vim.keymap.set('n', ',o', function()
---       require('git-conflict').choose 'ours'
---     end, { buffer = event.buf, desc = 'Choose ours', silent = true })
---     vim.keymap.set('n', ',t', function()
---       require('git-conflict').choose 'theirs'
---     end, { buffer = event.buf, desc = 'Choose theirs', silent = true })
---     vim.keymap.set('n', ',b', function()
---       require('git-conflict').choose 'both'
---     end, { buffer = event.buf, desc = 'Choose both', silent = true })
---     vim.keymap.set('n', ',n', function()
---       require('git-conflict').choose 'none'
---     end, { buffer = event.buf, desc = 'Choose none', silent = true })
---   end,
--- })
--- vim.api.nvim_create_autocmd('User', {
---   group = group,
---   pattern = 'GitConflictResolved',
---   callback = function(event)
---     vim.diagnostic.enable(true, { bufnr = event.buf })
---
---     for _, mapping in ipairs { ',o', ',t', ',b', ',n' } do
---       if vim.fn.hasmapto(mapping, 'n') > 0 then
---         vim.api.nvim_buf_del_keymap(event.buf, 'n', mapping)
---       end
---     end
---   end,
--- })
+    vim.keymap.set('n', ',o', function()
+      require('git-conflict').choose 'ours'
+    end, { buffer = event.buf, desc = 'Choose ours', silent = true })
+    vim.keymap.set('n', ',t', function()
+      require('git-conflict').choose 'theirs'
+    end, { buffer = event.buf, desc = 'Choose theirs', silent = true })
+    vim.keymap.set('n', ',b', function()
+      require('git-conflict').choose 'both'
+    end, { buffer = event.buf, desc = 'Choose both', silent = true })
+    vim.keymap.set('n', ',n', function()
+      require('git-conflict').choose 'none'
+    end, { buffer = event.buf, desc = 'Choose none', silent = true })
+  end,
+})
+vim.api.nvim_create_autocmd('User', {
+  group = group,
+  pattern = 'GitConflictResolved',
+  callback = function(event)
+    vim.diagnostic.enable(true, { bufnr = event.buf })
+
+    for _, mapping in ipairs { ',o', ',t', ',b', ',n' } do
+      if vim.fn.hasmapto(mapping, 'n') > 0 then
+        vim.api.nvim_buf_del_keymap(event.buf, 'n', mapping)
+      end
+    end
+  end,
+})
