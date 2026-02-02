4bee0632af901bf93c0a37f611c3513e06af52ef
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-02-02 18:00:00 -0300

    Adding dd to quickfix list

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -69,3 +69,21 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
     vim.opt_local.formatoptions:remove { 'o' }
   end,
 })
+
+-- Attach keymaps for quickfix list
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  desc = 'Attach keymaps for quickfix list',
+  callback = function()
+    vim.keymap.set('n', 'dd', function()
+      local qf_list = vim.fn.getqflist()
+      local current_line_number = vim.fn.line '.'
+      if qf_list[current_line_number] then
+        table.remove(qf_list, current_line_number)
+        vim.fn.setqflist(qf_list, 'r')
+        local new_line_number = math.min(current_line_number, #qf_list)
+        vim.fn.cursor(new_line_number, 1)
+      end
+    end, { buffer = true, noremap = true, silent = true, desc = 'Remove quickfix item under cursor' })
+  end,
+})
