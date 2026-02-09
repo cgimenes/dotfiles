03c5a7a4c9beee2e9822fa604eb6a336f5cf9bda
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-02-09 11:59:58 -0300

    Fix path-warp

--- nvim/.config/nvim/lua/plugins/path-warp/window.lua
+++ nvim/.config/nvim/lua/plugins/path-warp/window.lua
@@ -4,7 +4,7 @@ local M = {}
 ---@return number
 M.find_target_window = function()
   local available_windows = {}
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local buf = vim.api.nvim_win_get_buf(win)
     local bt = vim.bo[buf].buftype
     local ft = vim.bo[buf].filetype
