19d9fe0a0c15b83bcef52e57bf01f62c7aeee9e1
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-02-11 13:43:27 -0300

    Improving timelog aliases

--- zsh/.zsh/04-alias.zsh
+++ zsh/.zsh/04-alias.zsh
@@ -43,11 +43,11 @@ alias nest='npx --yes @nestjs/cli'
 alias tsx='npx --yes tsx'
 
 # Timelog
-alias tli='echo i `date "+%Y-%m-%d %H:%M:%S"` $* >>$TIMELOG_DIR/current.timeclock'
+alias tli='echo "\ni" `date "+%Y-%m-%d %H:%M:%S"` $* >>$TIMELOG_DIR/current.timeclock'
 alias tlo='echo o `date "+%Y-%m-%d %H:%M:%S"` >>$TIMELOG_DIR/current.timeclock'
-alias tll='tail $TIMELOG_DIR/current.timeclock'
 alias tle='nvim $TIMELOG_DIR/current.timeclock'
 alias tlb='hledger -f $TIMELOG_DIR/current.journal bal -D'
 alias tlbm='hledger -f $TIMELOG_DIR/current.journal bal -Xm -D'
 alias tlp='hledger -f $TIMELOG_DIR/current.journal print'
 alias tlpm='hledger -f $TIMELOG_DIR/current.journal print -Xm'
+alias tls='grep -E "^[io]" $TIMELOG_DIR/current.timeclock | tail -1 | grep "^i" && echo "Running" || echo "Stopped"'
