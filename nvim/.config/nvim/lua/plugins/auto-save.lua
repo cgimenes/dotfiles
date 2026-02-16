bf9f64901a0e66617e862bdc7393d23c3f7b090c
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-02-16 16:06:21 -0300

    Removing oil from auto-save

--- nvim/.config/nvim/lua/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/plugins/auto-save.lua
@@ -1,3 +1,14 @@
--- Auto save
+local excluded_filetypes = {
+  'gitcommit',
+  'oil',
+}
+
 vim.pack.add { 'https://github.com/okuuva/auto-save.nvim' }
-require('auto-save').setup {}
+require('auto-save').setup {
+  condition = function(buf)
+    if vim.tbl_contains(excluded_filetypes, vim.fn.getbufvar(buf, '&filetype')) then
+      return false
+    end
+    return true
+  end,
+}
