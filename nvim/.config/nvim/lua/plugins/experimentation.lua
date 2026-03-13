97bd25c6c834eaf91c604c273bbb6f4edf03bfde
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-03-13 16:31:15 -0300

    Trying render-markdown.nvim again

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -15,3 +15,16 @@ vim.pack.add {
   'https://github.com/NeogitOrg/neogit',
 }
 Map { '<leader>gG', '<cmd>Neogit<cr>', desc = 'Neogit' }
+
+-- Markdown
+-- vim.pack.add { 'https://github.com/OXY2DEV/markview.nvim' }
+vim.pack.add { 'https://github.com/MeanderingProgrammer/render-markdown.nvim' }
+--
+-- require('markview').setup {
+--   markdown = { enable = false },
+--   markdown_inline = { enable = false },
+-- }
+require('render-markdown').setup {
+  enabled = true,
+  anti_conceal = { enabled = false },
+}
