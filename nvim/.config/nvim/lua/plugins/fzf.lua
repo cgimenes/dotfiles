a7b5825940c479b9e04493162f44c51eca093a66
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-04-02 12:16:45 -0300

    Disabling fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,41 +1,41 @@
-vim.pack.add { 'https://github.com/ibhagwan/fzf-lua' }
-require('fzf-lua').setup {
-  { 'ivy', 'hide' },
-  winopts = {
-    preview = {
-      wrap = true,
-      hidden = true,
-    },
-  },
-  files = {
-    git_icons = false,
-    cwd_prompt = false,
-    prompt = ' ',
-    fzf_opts = { ['--scheme'] = 'path', ['--pointer'] = ' ' },
-  },
-  grep = {
-    rg_opts = '--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
-    RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH,
-    rg_glob = true, -- always parse glob patterns
-  },
-  keymap = {
-    builtin = {
-      ['<C-d>'] = 'preview-page-down',
-      ['<C-u>'] = 'preview-page-up',
-      ['<M-p>'] = 'toggle-preview',
-    },
-    fzf = {
-      ['ctrl-f'] = 'half-page-down',
-      ['ctrl-b'] = 'half-page-up',
-      ['ctrl-a'] = 'beginning-of-line',
-      ['ctrl-e'] = 'end-of-line',
-      ['ctrl-d'] = 'preview-page-down',
-      ['ctrl-u'] = 'preview-page-up',
-      ['ctrl-q'] = 'select-all+accept',
-      ['ctrl-y'] = 'accept',
-    },
-  },
-}
+-- vim.pack.add { 'https://github.com/ibhagwan/fzf-lua' }
+-- require('fzf-lua').setup {
+--   { 'ivy', 'hide' },
+--   winopts = {
+--     preview = {
+--       wrap = true,
+--       hidden = true,
+--     },
+--   },
+--   files = {
+--     git_icons = false,
+--     cwd_prompt = false,
+--     prompt = ' ',
+--     fzf_opts = { ['--scheme'] = 'path', ['--pointer'] = ' ' },
+--   },
+--   grep = {
+--     rg_opts = '--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
+--     RIPGREP_CONFIG_PATH = vim.env.RIPGREP_CONFIG_PATH,
+--     rg_glob = true, -- always parse glob patterns
+--   },
+--   keymap = {
+--     builtin = {
+--       ['<C-d>'] = 'preview-page-down',
+--       ['<C-u>'] = 'preview-page-up',
+--       ['<M-p>'] = 'toggle-preview',
+--     },
+--     fzf = {
+--       ['ctrl-f'] = 'half-page-down',
+--       ['ctrl-b'] = 'half-page-up',
+--       ['ctrl-a'] = 'beginning-of-line',
+--       ['ctrl-e'] = 'end-of-line',
+--       ['ctrl-d'] = 'preview-page-down',
+--       ['ctrl-u'] = 'preview-page-up',
+--       ['ctrl-q'] = 'select-all+accept',
+--       ['ctrl-y'] = 'accept',
+--     },
+--   },
+-- }
 -- Git
 -- Map { '<leader>gs', '<cmd>FzfLua git_status<cr>', desc = 'Git Status' }
 
