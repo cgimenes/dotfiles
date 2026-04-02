3ec0fbab25524e4bd1771629a762de3697c58142
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2026-04-02 11:59:48 -0300

    Removing nvim-unstack

--- nvim/.config/nvim/lua/plugins/experimentation.lua
+++ nvim/.config/nvim/lua/plugins/experimentation.lua
@@ -2,13 +2,6 @@
 vim.pack.add { 'https://github.com/hat0uma/csvview.nvim' }
 require('csvview').setup()
 
--- Unstack
-vim.pack.add { 'https://github.com/relf108/nvim-unstack' }
-require('nvim-unstack').setup {
-  mapkey = '<leader>U',
-  layout = 'tab',
-}
-
 -- Neogit
 vim.pack.add {
   'https://github.com/nvim-lua/plenary.nvim',
